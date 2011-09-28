07f9d044b0ddd9f7e67e50d884b72e802faa2db8
pob5000@gmail.com <pob5000@gmail.com@03fa84ad-ed8e-51c0-f0b2-510212dc3e91>
2011-09-28 21:05:42 +0000

    SGGTD : Snowy Grid Greenline Tower Defense version 1.0

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import pygame
+import random
  
 # Define some colors
 black    = (   0,   0,   0)
@@ -7,19 +8,49 @@ green    = (   0, 255,   0)
 red      = ( 255,   0,   0)
 
 pygame.init()
-  
+
 # Set the height and width of the screen
 size=[700,500]
 screen=pygame.display.set_mode(size)
- 
-pygame.display.set_caption("Tower Defense")
+
+# This sets the width and height of each grid location
+width=15
+height=15
+# This sets the margin between each cell
+margin=1
+
+# Create a 2 dimensional array. A two dimesional
+# array is simply a list of lists.
+grid=[]
+for row in range(3):
+    # Add an empty array that will hold each cell
+    # in this row
+    grid.append([])
+    for column in range(20):
+        grid[row].append(0) # Append a cell
+
+
+# Set the title of the game 
+pygame.display.set_caption("SGGTD : Snowy Grid Greenline Tower Defense")
  
 #Loop until the user clicks the close button.
 done=False
- 
+
 #Used to manage how fast the screen updates
 clock=pygame.time.Clock()
- 
+
+# Create an empty array
+star_list=[]
+
+# Loop 50 times and add a star in a random x,y position
+for i in range(50):
+    x=random.randrange(0,700)
+    y=random.randrange(0,500)
+    star_list.append([x,y])
+
+def draw_map(screen):
+    pygame.draw.line(screen,green,[50,250],[650,250],16)
+
 # -------- Main Program Loop -----------
 while done==False:
 
@@ -32,12 +63,36 @@ while done==False:
  
     # Set the screen background
     screen.fill(black)
-	
-    pygame.draw.line(screen,green,[0,0],[100,100],5)
- 
-    # Go ahead and update the screen with what we've drawn.
+    
+    # Process each star in the list
+    for i in range(len(star_list)):
+        # Draw the star
+        pygame.draw.circle(screen,white,star_list[i],2)
+
+        # Move the star down one pixel
+        star_list[i][1]+=1
+         
+        # If the star has moved off the bottom of the screen
+        if star_list[i][1] > 500:
+            # Reset it just above the top
+            y=random.randrange(-50,-10)
+            star_list[i][1]=y
+            # Give it a new x position
+            x=random.randrange(0,700)
+            star_list[i][0]=x
+
+    # Draw the map
+    draw_map(screen)
+    
+    # Draw the grid
+    for row in range(3):
+        for column in range(20):
+            color = white
+            if grid[row][column] == 1:
+                color = green
+            pygame.draw.rect(screen,color,[(margin+width)*column+margin+50,(margin+height)*row+margin+100,width,height])
+
+	# update the screen
     pygame.display.flip()
-     
-# Be IDLE friendly. If you forget this line, the program will 'hang'
-# on exit.
+
 pygame.quit ()
